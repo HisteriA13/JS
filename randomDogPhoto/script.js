d5ec50c748b3c2b92a676c799454eb71c3a1950f
Hilda Hadzic <13hilda@gmail.com>
2026-08-09 19:01:24 +0200

    Add script.js

---
+++ randomDogPhoto/script.js
@@ -0,0 +1,4 @@
+const btnEl = document.getElementById("btn");
+const imgEl = document.querySelector(".dog-img");
+const dogContainerEl = document.querySelector(".dog-container");
+const dogBreedEl = document.querySelector(".dog-breed");
