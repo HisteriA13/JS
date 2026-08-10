bd4e146ed6181258853cc4167fe6beea643873ed
Hilda Hadzic <13hilda@gmail.com>
2026-08-10 13:13:54 +0200

    Testing script.js

--- randomDogPhoto/script.js
+++ randomDogPhoto/script.js
@@ -2,3 +2,31 @@ const btnEl = document.getElementById("btn");
 const imgEl = document.querySelector(".dog-img");
 const dogContainerEl = document.querySelector(".dog-container");
 const dogBreedEl = document.querySelector(".dog-breed");
+
+btnEl.addEventListener("click", getPhoto);
+
+async function getPhoto() {
+  try {
+    btnEl.disabled = true;
+    btnEl.textContent = "Loading...";
+    imgEl.src = "img/loading.svg";
+
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    console.log(data);
+
+    btnEl.disabled = false;
+    btnEl.textContent = "Woof!";
+    dogContainerEl.style.display = "block";
+    imgEl.src = data.message;
+
+    const urlParts = data.message.split("/");
+    const breedName = urlParts[4].replace("-", " ");
+    dogBreedEl.textContent =
+      breedName.charAt(0).toUpperCase() + breedName.slice(1).toUpperCase();
+  } catch (error) {
+    btnEl.disabled = false;
+    btnEl.textContent = "Woof!";
+    dogBreedEl.textContent = "An error occured...";
+  }
+}
