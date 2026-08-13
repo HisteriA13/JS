3b7eac3764bbfbd07f4cf2b00f0a36526a328d6d
Hilda Hadzic <13hilda@gmail.com>
2026-08-13 14:22:05 +0200

    Finish

--- loadingBlur/script.js
+++ loadingBlur/script.js
@@ -4,3 +4,16 @@ const perEl = document.querySelector(".percentage");
 const remap = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+let load = 0;
+let interval = setInterval(loadingBlur, 40); // smaller number = faster
+
+function loadingBlur() {
+  load++;
+  if (load > 99) {
+    clearInterval(interval);
+  }
+  perEl.textContent = `${load}%`;
+  perEl.style.opacity = remap(load, 80, 100, 1, 0);
+  bgEl.style.filter = `blur(${remap(load, 0, 100, 20, 0)}px)`;
+}
